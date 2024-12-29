1862f2806a26a0ea6022662b6e6fe0bf8e8f5fd9
pravalgautam <71179696+pravalgautam@users.noreply.github.com>
2024-12-29 11:58:43 +0530

    Initial Commit

---
+++ InternAssignment/InternAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternAssignmentApp.swift
+//  InternAssignment
+//
+//  Created by Praval Gautam on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InternAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
